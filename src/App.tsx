cf05741588d27e2454a840404d394f5d9b636070
davimbastos <davimbastos@gmail.com>
2020-07-18 18:21:05 -0300

    Criação do projeto e  adição dacoluna de listagem de servidores

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlobalSyles from './styles/GlobalStyles'
+
+function App() {
+  return (
+    <>
+
+      <Layout />
+
+      <GlobalSyles />
+
+    </>
+  ) 
+}
+
+export default App;
